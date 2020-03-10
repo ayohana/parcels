efaa2e660c76843ae946df8dbfadaab48086191d
Adela Darmansyah <adela.yohana@gmail.com>
2020-03-10 14:00:48 -0700

    Create Parcel constructor
    Co-authored-by: Andriy Veremyeyev <belyybrat@gmail.com>

--- Shipping.Solution/Shipping/Models/Parcel.cs
+++ Shipping.Solution/Shipping/Models/Parcel.cs
@@ -7,12 +7,36 @@ namespace Shipping.Models
   {
     public int Length {get; set;}
     public int Width {get; set;}
-    public int Height {get; set}
-    public int Weight {get; set}
-
-    public Parcel()
+    public int Height {get; set;}
+    public int Weight {get; set;}
+    public string Destination{get; set;}
+    public Dictionary <string, int> Cities = new Dictionary <string, int>()
     {
+      {"Atlanta", 2675},
+      {"Boston", 3065},
+      {"Chicago", 2070},
+      {"Clevelend", 2421},
+      {"Dallas", 2195},
+      {"Denver", 1321},
+      {"Detroit", 2357},
+      {"Houston", 2442},
+      {"Las Vegas", 1122},
+      {"Los Angeles", 1141},
+      {"New York", 2890},
+      {"Philadelphia ", 2840}
+    };
 
+    public Parcel(int length, int width, int height, int weight)
+    {
+      Length = length;
+      Width = width;
+      Height = height;
+      Weight = weight;
+    }
+
+    public int Volume()
+    {
+      return Length * Width * Height;
     }
 
   }
